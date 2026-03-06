8c9b1a3c99470e8e1a60aee50138cfd2e2c193f4
k-satya-2400031153 <k.satyartx4050@gmail.com>
2026-03-06 23:38:25 +0530

    Added Skill Experiment 8: Spring Boot JPQL and Query Methods

---
+++ Skill-Experiment-8/src/main/java/com/klu/productmodule/SkillExperiment8Application.java
@@ -0,0 +1,13 @@
+package com.klu.productmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillExperiment8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillExperiment8Application.class, args);
+    }
+
+}
