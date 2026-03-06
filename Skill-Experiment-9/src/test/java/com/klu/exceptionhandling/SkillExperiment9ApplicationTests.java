3cd4831d20f12cb879d00f41c4feee1806f98a11
k-satya-2400031153 <k.satyartx4050@gmail.com>
2026-03-07 00:05:57 +0530

    Added Skill Experiment 9: Global Exception Handling with ControllerAdvice

---
+++ Skill-Experiment-9/src/test/java/com/klu/exceptionhandling/SkillExperiment9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klu.exceptionhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillExperiment9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
